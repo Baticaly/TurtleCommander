77c98065a23774d9c0585f5421a129a340812964
Baticaly <ta6nbe@gmail.com>
2023-02-07 23:30:20 +0100

    supabase auth

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,23 @@
 <script>
 	import '../app.css';
+	import { supabase } from '$lib/supabaseClient';
+	import { invalidate } from '$app/navigation';
+	import { onMount } from 'svelte';
+	import './style.css';
+
+	onMount(() => {
+		const {
+			data: { subscription }
+		} = supabase.auth.onAuthStateChange(() => {
+			invalidate('supabase:auth');
+		});
+
+		return () => {
+			subscription.unsubscribe();
+		};
+	});
 </script>
 
-<slot />
+<div class="container" style="padding: 50px 0 100px 0">
+	<slot />
+</div>
