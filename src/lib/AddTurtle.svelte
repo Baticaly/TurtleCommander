658741fbceea697850620e1132abef6e9bfd3307
Baticaly <ta6nbe@gmail.com>
2023-02-09 19:59:41 +0100

    dashboard final

--- src/lib/AddTurtle.svelte
+++ src/lib/AddTurtle.svelte
@@ -1,11 +1,26 @@
 <script>
-	function addTurtle() {}
+	// Supabase
+	let turtleList = [{ id: 'Turtle #1' }];
+
+	function addTurtle() {
+		let turtleID = 'Turtle #' + JSON.stringify(turtleList.length + 1);
+		turtleList = [...turtleList, { id: turtleID }];
+		console.log(turtleList);
+	}
 </script>
 
+{#each turtleList as turtle}
+	<div
+		class="text-2xl text-buttonTextSecondary bg-buttonBackgroundSecondary py-2.5 w-11/12 m-auto mb-2 text-center rounded-2xl hover:bg-buttonBackgroundSecondaryHover hover:cursor-pointer"
+	>
+		{turtle.id}
+	</div>
+{/each}
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
 <div
-	class="text-2xl text-headline text-center mx-auto py-0.5 w-9 rounded-full bg-buttonBackground hover:bg-buttonBackgroundHover hover:cursor-pointer"
+	class="text-2xl text-headline text-center mx-auto py-0.5 w-9 rounded-full bg-buttonBackground hover:bg-buttonBackgroundHover hover:cursor-pointer mb-2"
 	on:click={addTurtle}
-	on:keypress={null}
 >
 	+
 </div>
--- src/routes/Dashboard.svelte
+++ src/routes/Dashboard.svelte
@@ -1,7 +1,7 @@
 <script>
-	import { onMount } from "svelte";
-	import { supabase } from "$lib/supabaseClient";
-	import AddTurtle from "$lib/AddTurtle.svelte";
+	import { onMount } from 'svelte';
+	import { supabase } from '$lib/supabaseClient';
+	import AddTurtle from '$lib/AddTurtle.svelte';
 
 	export let session;
 
@@ -20,9 +20,9 @@
 			const { user } = session;
 
 			const { data, error, status } = await supabase
-				.from("profiles")
+				.from('profiles')
 				.select(`username, website, avatar_url`)
-				.eq("id", user.id)
+				.eq('id', user.id)
 				.single();
 
 			if (data) {
@@ -51,10 +51,10 @@
 				username,
 				website,
 				avatar_url: avatarUrl,
-				updated_at: new Date(),
+				updated_at: new Date()
 			};
 
-			let { error } = await supabase.from("profiles").upsert(updates);
+			let { error } = await supabase.from('profiles').upsert(updates);
 
 			if (error) throw error;
 		} catch (error) {
@@ -81,21 +81,21 @@
 	}
 
 	function toUserInfo() {
-		var userInfo = document.getElementById("userInfo");
-		var dashboard = document.getElementById("dashboard");
-		userInfo.classList.remove("hidden");
-		dashboard.classList.remove("flex");
-		dashboard.classList.add("hidden");
-		userInfo.classList.add("flex");
+		var userInfo = document.getElementById('userInfo');
+		var dashboard = document.getElementById('dashboard');
+		userInfo.classList.remove('hidden');
+		dashboard.classList.remove('flex');
+		dashboard.classList.add('hidden');
+		userInfo.classList.add('flex');
 	}
 
 	function toDashboard() {
-		var userInfo = document.getElementById("userInfo");
-		var dashboard = document.getElementById("dashboard");
-		userInfo.classList.add("hidden");
-		dashboard.classList.add("flex");
-		dashboard.classList.remove("hidden");
-		userInfo.classList.remove("flex");
+		var userInfo = document.getElementById('userInfo');
+		var dashboard = document.getElementById('dashboard');
+		userInfo.classList.add('hidden');
+		dashboard.classList.add('flex');
+		dashboard.classList.remove('hidden');
+		userInfo.classList.remove('flex');
 	}
 </script>
 
@@ -132,7 +132,7 @@
 			<input
 				type="submit"
 				class="bg-connectBackground w-full rounded-2xl border-2 border-loginCardBorder p-4 text-headline cursor-pointer hover:bg-buttonBackgroundHover"
-				value={loading ? "Loading..." : "Update"}
+				value={loading ? 'Loading...' : 'Update'}
 				disabled={loading}
 			/>
 		</div>
@@ -156,32 +156,21 @@
 	<!-- Dashboard -->
 	<div class="flex flex-row w-11/12 h-5/6 justify-around" id="dashboard">
 		<!-- Turtle List -->
-		<div class="flex flex-col basis-23p bg-headline rounded-3xl">
+		<div class="flex flex-col basis-[23%] bg-headline rounded-3xl">
 			<div class="flex flex-row items-center justify-center pt-5 pb-4">
 				<div class="basis-1/6">
-					<img
-						src="src/assets/logo2.svg"
-						alt="Logo2"
-						class="transform -scale-x-100"
-					/>
+					<img src="src/assets/logo2.svg" alt="Logo2" class="transform -scale-x-100" />
 				</div>
 				<div class="font-extrabold text-2xl pl-3">TurtleCommander</div>
 			</div>
-			<div
-				class="bg-usercardBackground h-0.5 w-11/12 rounded-full mx-auto mb-5"
-			/>
-			<div class="h-3/4 overflow-scroll">
-				<div
-					class="text-2xl text-buttonTextSecondary bg-buttonBackgroundSecondary py-2.5 w-11/12 m-auto mb-2 text-center rounded-2xl hover:bg-buttonBackgroundSecondaryHover hover:cursor-pointer"
-				>
-					Turtle #1
-				</div>
+			<div class="bg-usercardBackground h-0.5 w-11/12 rounded-full mx-auto mb-4" />
+			<div class="h-[80%] overflow-auto">
 				<AddTurtle />
 			</div>
+			<!-- svelte-ignore a11y-click-events-have-key-events -->
 			<div
-				class="flex flex-row top-3/4 py-4 items-center bg-loginCardBorder rounded-b-3xl"
+				class="flex flex-row top-3/4 py-4 items-center bg-loginCardBorder rounded-b-3xl mt-auto cursor-pointer"
 				on:click={toUserInfo}
-				on:keypress={null}
 			>
 				<div class="ml-6 h-8 w-8 rounded-full bg-headline">
 					<img src="" alt="" />
@@ -194,9 +183,3 @@
 		<div class="basis-3/4 bg-headline rounded-3xl" />
 	</div>
 </div>
-
-<style>
-	.basis-23p {
-		flex-basis: 23%;
-	}
-</style>
