d98ac5859d54101aa5ad7ad39a118345a7de4257
Baticaly <ta6nbe@gmail.com>
2023-02-11 15:08:13 +0100

    turtle svelte

---
+++ src/lib/Turtle.svelte
@@ -0,0 +1,9 @@
+<script>
+	export let turtle;
+</script>
+
+<div
+	class="text-2xl text-buttonTextSecondary bg-buttonBackgroundSecondary py-2.5 w-11/12 m-auto mb-2 text-center rounded-2xl hover:bg-buttonBackgroundSecondaryHover hover:cursor-pointer"
+>
+	{turtle.turtlename}
+</div>
