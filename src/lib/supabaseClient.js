7661774afd0f78848c86ad763bf3f1e495acb2a1
Baticaly <ta6nbe@gmail.com>
2023-02-25 21:49:44 +0100

    env method change

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,4 +1,4 @@
 import { createClient } from '@supabase/auth-helpers-sveltekit'
-import { VITE_PUBLIC_SUPABASE_URL, VITE_PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public'
+import { SUPABASE_URL, SUPABASE_ANON_KEY } from '$lib/Env';
 
-export const supabase = createClient(VITE_PUBLIC_SUPABASE_URL, VITE_PUBLIC_SUPABASE_ANON_KEY)
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
