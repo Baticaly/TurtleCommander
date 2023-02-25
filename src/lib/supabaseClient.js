51e1c8d0c9594cfee0d46a17e21052680b8b2203
Baticaly <ta6nbe@gmail.com>
2023-02-25 21:53:18 +0100

    env loader change

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,4 +1,4 @@
 import { createClient } from '@supabase/auth-helpers-sveltekit'
-import { SUPABASE_URL, SUPABASE_ANON_KEY } from '$lib/Env';
+import { SUPABASE_URL, SUPABASE_ANON_KEY } from '$lib/env';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
